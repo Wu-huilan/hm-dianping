c20fc46535ede964f81b65d0fd0ada71f28fbbf4
bella <779790228@qq.com>
2022-09-08 00:01:42 +0800

    全局唯一id

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,40 @@
 package com.hmdp;
 
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    RedisIdWorker redisIdWorker;
+
+    ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(redisIdWorker.nextId("order"));
+
+            }
+            latch.countDown();
+        };
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time="+(end-start));
+    }
 
 
 }
